e5c70e7108ef8dfa22dc4b480b01529048f1b248
Yuhang <gu_yuhang@outlook.com>
2019-06-07 00:32:01 +0200

    1

---
+++ 3D/js/main.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Aero on 15/02/2017.
+ */
+
+var dataFlows;
+var dataChord;
+var dataChordT;
+var dataChordC;
+
+
+var flowsSet = [];
+var legendSet = [];
+
+
+const SHOW_ALL_FLOWS = -5;
+var font = null;
+
+const DEGS_TO_RADS = Math.PI / 180,
+    UNIT_SIZE = 100;
+
+const DIGIT_0 = 48,
+    DIGIT_9 = 57,
+    COMMA = 44,
+    SPACE = 32,
+    PERIOD = 46,
+    MINUS = 45;
+
+
+function visStart(e) {
+
+    loadData(function() {
+
+        vis3DFlowMap();
+    });
+
+
+    $('input[name="municipality"]').change(function(e) {
+
+        //cancelAnimationFrame(this.id);
+
+        document.getElementById("map").removeChild(VIS.cssRenderer.domElement);
+        document.getElementById("map").removeChild(VIS.glRenderer.domElement);
+
+        if ($('input[name="municipality"]:checked').val() == "Utrecht") {
+            citySelectedCurr = "Utrecht";
+        }
+        if ($('input[name="municipality"]:checked').val() == "Friesland") {
+
+            citySelectedCurr = "Leeuwarden";
+        }
+        if ($('input[name="municipality"]:checked').val() == "Groningen") {
+
+            citySelectedCurr = "Groningen";
+        }
+
+
+        loadData(function() {
+            vis3DFlowMap();
+        });
+
+
+    });
+}
